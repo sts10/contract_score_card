28281dad2ed230fa77d0cf2e5739cfa3f42421bf
sts10 <sschlinkert@gmail.com>
2014-05-19 15:07:33 -0400

    basic workings in place

---
+++ js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  // alert('test');
+
+  $('input[type=text]').on("keyup", function(){
+    calculateAll();
+  });
+
+  function getHandScore(player, hand){
+    var id = "#player" + player + "_hand" + hand;
+    var value = $(id).val();
+    if(value) {
+      return parseInt(value);
+    }
+    else {
+      return 0;
+    }
+  }
+
+  function makeScoreArray(player){
+    var array = [getHandScore(player, 1), getHandScore(player, 2)];
+    return array;
+  }
+
+  function sumArray(array){
+    return array.reduce(function(pv, cv) { return pv + cv; }, 0);
+  }
+
+  function calculatePlayerTotal(player_num){
+    var player_array = makeScoreArray(player_num);
+    var id = "#player" + player_num + "_total";
+
+    $(id).html(sumArray(player_array));
+  }
+
+  function calculateAll(){
+    calculatePlayerTotal(1);
+    calculatePlayerTotal(2);
+  }
+});
+
