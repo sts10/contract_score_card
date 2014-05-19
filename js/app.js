4fb47c8b92457f12b12771dde8ff21ffc5a80248
sts10 <sschlinkert@gmail.com>
2014-05-19 18:03:56 -0400

    works pretty well now that I remembered to reset low_score

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
   // alert('test');
 
+  var low_score = 10000;
+  var leader = 0;
+
+
   $('input[type=text]').on("keyup", function(){
-    calculateAll();
+    var player_number = ($(this).attr('id')).charAt(6);
+    low_score = 10000;
+    
+    if (playerHasName(player_number)) {
+      calculatePlayerTotal(player_number); 
+      postPlayerTotal(player_number);
+    }
+
+    findCurrentLeader();
+    highlightHighScore();
   });
 
   function getHandScore(player, hand){
@@ -34,18 +47,60 @@ $(document).ready(function(){
 
   function calculatePlayerTotal(player_num){
     var player_array = makeScoreArray(player_num);
-    var id = "#player" + player_num + "_total";
-
-    $(id).html(sumArray(player_array));
+    var player_total = sumArray(player_array);
+    return player_total;
   }
 
-  function calculateAll(){
+  function postPlayerTotal(player_num){
+    var player_total = calculatePlayerTotal(player_num);
+    var id = "#player" + player_num + "_total";
+
+    $(id).html(player_total);
+  }
+
+
+  function findCurrentLeader(){
     var i = 1;
+
     while(i<9){
-      calculatePlayerTotal(i);  
+      if (playerHasName(i)) {
+        var this_player_score = calculatePlayerTotal(i);
+        console.log("I'm going through the loop and this player's score is " + this_player_score);
+        if (this_player_score < low_score){  
+          low_score = this_player_score;
+          leader = i;
+        }
+      }
       i = i + 1;
     }
-   
+    console.log('I found the leader and it is player '+ leader + ' with score of ' + low_score);
+    // return [leader, high_ score];
+  }
+
+  function highlightHighScore(){
+
+    // remove background color from old leader
+    
+    $('.name').css("background-color", "transparent");
+    $('.name').parent().css("background-color", "transparent");
+    $('.total').css("background-color", "transparent");
+    
+    var name_id = "#player" + leader+"_name";
+    var total_id = "#player" + leader+"_total";
+
+    $(name_id).css("background-color", "#FFFF99");
+    $(name_id).parent().css("background-color", "#FFFF99");
+    $(total_id).css("background-color", "#FFFF99");
+  }
+
+  function playerHasName(player_number){
+    var id = "#player" + player_number + "_name";
+    if ($(id).val().trim().length >= 1) {
+      return true;
+    } else {
+      return false;
+    }
+
   }
 
  
---
+++ js/junk.js
@@ -0,0 +1,8 @@
+
+function calculateAll(){
+  var i = 1;
+  while(i<9){
+    calculatePlayerTotal(i);  
+    i = i + 1;
+  }
+}
