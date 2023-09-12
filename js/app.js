85de2c12a13e96b565289ee476ba0caaed88cc85
Sam Schlinkert <sschlinkert@gmail.com>
2023-09-11 23:27:59 -0400

    re-declares most variables to let or const, based on what deno told me

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-  var number_of_players = 8;
-  var number_of_hands = 5;
-  var max_name_length_before_rotate = 7;
+  const number_of_players = 8;
+  const number_of_hands = 5;
+  const max_name_length_before_rotate = 7;
 
-  var low_score = 10000;
-  var leader = 0;
+  let low_score = 10000;
+  let leader = 0;
 
   $("input[type=text]").on("keyup", function () {
-    var player_number = ($(this).attr("id")).charAt(6);
+    const player_number = ($(this).attr("id")).charAt(6);
     low_score = 10000;
 
     if ($(this).val().indexOf("*") > -1) {
-      var hand_number = ($(this).attr("id")).charAt(12);
+      const hand_number = ($(this).attr("id")).charAt(12);
       $(this).val(getPenaltyScoreForPlayer(player_number, hand_number));
     }
 
@@ -36,17 +36,17 @@ $(document).ready(function () {
   });
 
   function getHandScore(player, hand) {
-    var value = $("#player" + player + "_hand" + hand).val();
+    const value = $("#player" + player + "_hand" + hand).val();
     return value ? parseInt(value) : 0;
   }
 
   function getPenaltyScoreForPlayer(penalized_player, hand) {
-    var i = 1;
-    var highest_score_of_hand = 0;
+    let i = 1;
+    let highest_score_of_hand = 0;
 
     while (i <= number_of_players) {
       if (playerHasName(i) && i !== penalized_player) {
-        var this_player_score = getHandScore(i, hand);
+        const this_player_score = getHandScore(i, hand);
         if (this_player_score > highest_score_of_hand) {
           highest_score_of_hand = this_player_score;
         }
@@ -57,8 +57,8 @@ $(document).ready(function () {
   }
 
   function makeScoreArray(player) {
-    var array = [];
-    var i = 1;
+    const array = [];
+    let i = 1;
 
     while (i <= number_of_hands) {
       array.push(getHandScore(player, i));
@@ -83,11 +83,11 @@ $(document).ready(function () {
   }
 
   function updateCurrentLeader() {
-    var i = 1;
+    let i = 1;
 
     while (i <= number_of_players) {
       if (playerHasName(i)) {
-        var this_player_score = calculatePlayerTotal(i);
+        const this_player_score = calculatePlayerTotal(i);
         if (this_player_score < low_score) {
           low_score = this_player_score;
           leader = i;
@@ -103,8 +103,8 @@ $(document).ready(function () {
     $(".total_row").css("background-color", "transparent");
 
     // set local variables to the css ids of the new leader
-    var name_id = "#player" + leader + "_name";
-    var total_id = "#player" + leader + "_total";
+    const name_id = "#player" + leader + "_name";
+    const total_id = "#player" + leader + "_total";
 
     $(name_id).parent().css("background-color", "#FFFF99");
     $(total_id).css("background-color", "#FFFF99");
@@ -115,11 +115,11 @@ $(document).ready(function () {
   }
 
   function getLongestNameLength() {
-    var i = 1;
-    var longest_name_length = 0;
+    let i = 1;
+    let longest_name_length = 0;
     while (i <= number_of_players) {
       if (playerHasName(i)) {
-        var this_player_name = $("#player" + i + "_name").val();
+        const this_player_name = $("#player" + i + "_name").val();
         if (this_player_name.length > longest_name_length) {
           longest_name_length = this_player_name.length;
         }
